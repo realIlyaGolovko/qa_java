30fb9ecbe35828f66c1f1f5913b512348a8a7db1
realIlyaGolovko <real-golovko-ilya@yandex.ru>
2022-02-12 18:24:59 +0300

    Added LiomParameterizedTest class

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -4,9 +4,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
